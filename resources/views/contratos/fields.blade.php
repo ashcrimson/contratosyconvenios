e4b3f8898833564ae7e9f45990dcec6bea9cf4e0
Edras Altamirano <altamiranoesdras@gmail.com>
2021-07-27 11:48:22 -0600

    Mejoras formulario contratos

--- resources/views/contratos/fields.blade.php
+++ resources/views/contratos/fields.blade.php
@@ -1,13 +1,13 @@
 <div class="form-row" id="fieldsContratos">
 
     <!-- Id Mercado Publico Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('id_mercado_publico', 'Id Mercado Publico:') !!}
         {!! Form::text('id_mercado_publico', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
     </div>
 
     <!-- Tipo Id Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('tipo_id', 'Tipo') !!}
 
         <multiselect v-model="tipo" :options="tipos" label="nombre" placeholder="Seleccione uno...">
@@ -16,50 +16,52 @@
     </div>
 
     <!-- Licitacion Id Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         <select-licitacion :items="licitaciones" v-model="licitacion" label="Licitacion"></select-licitacion>
     </div>
 
     <!-- Proveedor Id Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         <select-proveedor :items="proveedores" v-model="proveedor" label="Proveedor"></select-proveedor>
     </div>
 
     <!-- Cargo Id Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         <select-cargo :items="cargos" v-model="cargo" label="Cargo"></select-cargo>
     </div>
 
     <!-- Moneda Id Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         <select-moneda :items="monedas" v-model="moneda" label="Moneda"></select-moneda>
     </div>
 
     <!-- Monto Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('monto', 'Monto:') !!}
         {!! Form::number('monto', null, ['class' => 'form-control']) !!}
+
+        <input type="hidden" name="estado_alerta" value="0">
     </div>
 
     <!-- Estado Alerta Field -->
-    <div class="form-group col-sm-6">
-        {!! Form::label('estado_alerta', 'Estado Alerta:') !!}
-        <label class="checkbox-inline">
-            <input type="hidden" name="estado_alerta" value="0">
-            {!! Form::checkbox('estado_alerta', '1', null) !!}
-        </label>
-    </div>
+{{--    <div class="form-group col-sm-4">--}}
+{{--        {!! Form::label('estado_alerta', 'Estado Alerta:') !!}--}}
+{{--        <label class="checkbox-inline">--}}
+
+{{--            {!! Form::checkbox('estado_alerta', '1', null) !!}--}}
+{{--        </label>--}}
+{{--    </div>--}}
 
 
     <!-- Fecha Inicio Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('fecha_inicio', 'Fecha Inicio:') !!}
         {!! Form::date('fecha_inicio', onlyDate($contrato->fecha_inicio ?? null), ['class' => 'form-control','id'=>'fecha_inicio']) !!}
     </div>
 
 
     <!-- Fecha Termino Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('fecha_termino', 'Fecha Termino:') !!}
         {!! Form::date('fecha_termino', onlyDate($contrato->fecha_termino ?? null), ['class' => 'form-control','id'=>'fecha_termino']) !!}
     </div>
@@ -67,14 +69,14 @@
 
 
     <!-- Fecha Aprobacion Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('fecha_aprobacion', 'Fecha Aprobacion:') !!}
         {!! Form::date('fecha_aprobacion', onlyDate($contrato->fecha_aprobacion ?? null), ['class' => 'form-control','id'=>'fecha_aprobacion']) !!}
     </div>
 
 
     <!-- Fecha Alerta Vencimiento Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('fecha_alerta_vencimiento', 'Fecha Alerta Vencimiento:') !!}
         {!! Form::date('fecha_alerta_vencimiento', onlyDate($contrato->fecha_alerta_vencimiento ?? null), ['class' => 'form-control','id'=>'fecha_alerta_vencimiento']) !!}
     </div>
@@ -83,31 +85,31 @@
     <!-- Objeto Field -->
     <div class="form-group col-sm-12 col-lg-12">
         {!! Form::label('objeto', 'Objeto:') !!}
-        {!! Form::textarea('objeto', null, ['class' => 'form-control']) !!}
+        {!! Form::textarea('objeto', null, ['class' => 'form-control','rows' => 2]) !!}
     </div>
 
     <!-- Numero Boleta Garantia Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('numero_boleta_garantia', 'Numero Boleta Garantia:') !!}
         {!! Form::text('numero_boleta_garantia', null, ['class' => 'form-control','maxlength' => 45,'maxlength' => 45]) !!}
     </div>
 
     <!-- Monto Boleta Garantia Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('monto_boleta_garantia', 'Monto Boleta Garantia:') !!}
         {!! Form::number('monto_boleta_garantia', null, ['class' => 'form-control']) !!}
     </div>
 
 
     <!-- Fecha Vencimiento Boleta Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('fecha_vencimiento_boleta', 'Fecha Vencimiento Boleta:') !!}
         {!! Form::date('fecha_vencimiento_boleta', onlyDate($contrato->fecha_vencimiento_boleta ?? null), ['class' => 'form-control','id'=>'fecha_vencimiento_boleta']) !!}
     </div>
 
 
     <!-- Alerta Vencimiento Boleta Field -->
-    <div class="form-group col-sm-6">
+    <div class="form-group col-sm-4">
         {!! Form::label('alerta_vencimiento_boleta', 'Alerta Vencimiento Boleta:') !!}
         {!! Form::date('alerta_vencimiento_boleta', onlyDate($contrato->alerta_vencimiento_boleta ?? null), ['class' => 'form-control','id'=>'alerta_vencimiento_boleta']) !!}
     </div>
