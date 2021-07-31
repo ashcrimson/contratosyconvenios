e4c1c325247cf95668454f29fb1fa13e898d8253
Edras Altamirano <altamiranoesdras@gmail.com>
2021-07-31 11:40:42 -0600

    Nueva migracion para documentos

---
+++ database/migrations/2021_07_30_214552_create_documentos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('model');
+            $table->string('name')->nullable();
+            $table->string('file_name');
+            $table->string('mime_type')->nullable();
+            $table->unsignedBigInteger('size');
+            $table->binary('data')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documentos');
+    }
+}
