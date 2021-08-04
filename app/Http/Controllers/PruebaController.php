fab6af6d6723fd9b1cebf9cf5ec4a0786eecd8bd
Edras Altamirano <altamiranoesdras@gmail.com>
2021-08-04 14:38:33 -0600

    PruebaController.php prueba api monedas curl y http

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PruebaController extends Controller
 {
     public function index()
     {
-        return "hola";
+
+        if (request()->api_monedas==1){
+
+            $uriApi = "https://mindicador.cl/api";
+
+            $res = Http::get($uriApi)->json();
+
+            return $res;
+        }
+
+        if (request()->api_monedas==2){
+            $apiUrl = 'https://mindicador.cl/api';
+            //Es necesario tener habilitada la directiva allow_url_fopen para usar file_get_contents
+            if ( ini_get('allow_url_fopen') ) {
+                $json = file_get_contents($apiUrl);
+            } else {
+                //De otra forma utilizamos cURL
+                $curl = curl_init($apiUrl);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $json = curl_exec($curl);
+                curl_close($curl);
+            }
+
+            $dailyIndicators = json_decode($json);
+
+            dd($dailyIndicators);
+
+        }
+
+
     }
 }
