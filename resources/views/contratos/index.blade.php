0873f6df76082bec0ead6490b56ebe56818c85b0
Felipe Pino <felipepinoredes@gmail.com>
2021-09-15 12:20:48 -0300

    cambios sin mucha importancia 2

--- resources/views/contratos/index.blade.php
+++ resources/views/contratos/index.blade.php
@@ -14,11 +14,13 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-right">
                         <li class="breadcrumb-item">
+                            @can('Crear Contratos')
                             <a class="btn btn-outline-success"
                                 href="{!! route('contratos.create') !!}">
                                 <i class="fa fa-plus"></i>
                                 <span class="d-none d-sm-inline">{{__('Nuevo')}}</span>
                             </a>
+                            @endcan
                         </li>
                     </ol>
                 </div>
