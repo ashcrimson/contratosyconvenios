c8b28ff48395e4858497ac86d26916d1d86e96f3
Josue Martinez <m.josue3443@gmail.com>
2022-05-05 09:46:57 -0600

    se crean migraciones para los mantenimientos de la ornde de pago de tabla nueva

---
+++ database/migrations/2022_05_05_085433_create_formas_pagos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormasPagosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formas_pagos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('valor');
+            $table->string('descripcion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formas_pagos');
+    }
+}
